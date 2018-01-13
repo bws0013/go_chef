a7ec58e3ca06548e20fe11caa92d9773d676d31c
Ben <i@auburn.edu>
2018-01-13 11:16:31 -0600

    Figure out editing bash profile

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,13 +10,21 @@ node.default['go']['platform'] = node['kernel']['machine'] =~ /i.86/ ? '386' : '
 node.default['go']['filename'] = "go#{node['go']['version']}.#{node['os']}-#{node['go']['platform']}.tar.gz"
 node.default['go']['url'] = "https://golang.org/dl/#{node['go']['filename']}"
 
-execute 'print_stuff' do
-  command "echo #{node.default['go']['url']}"
+directory "/projects/" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
 end
 
-execute 'print_stuff' do
-  command "echo #{node.default['go']['platform']}"
-end
+
+# execute 'print_stuff' do
+#   command "echo #{node.default['go']['url']}"
+# end
+
+# execute 'print_stuff' do
+#   command "echo #{node.default['go']['platform']}"
+# end
 
 # if node.default['go']['from_source'] == false
 #   directory "#{node['go']['install_dir']}/go" do
@@ -34,6 +42,85 @@ end
 tar_extract "#{node.default['go']['url']}" do
   target_dir '/usr/local'
 end
+
+# file '/etc/profile.d/path.sh' do
+#   content 'export PATH=$PATH:/usr/local/go/bin'
+#   mode '0755'
+# end
+
+directory '/projects/bin' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/projects/pkg' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/projects/src' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+unless File.exist?('/etc/profile.d/path.sh')
+  file '/etc/profile.d/path.sh'
+  ruby_block "profile.d" do
+    block do
+      line = 'export PATH=$PATH:/usr/local/go/bin'
+      file = Chef::Util::FileEdit.new("/etc/profile.d/path.sh")
+      file.insert_line_if_no_match("/#{line}/", "export PATH=$PATH:/usr/local/go/bin")
+      file.write_file
+    end
+  end
+end
+
+unless File.exist?('/etc/profile.d/path.sh')
+  file '/etc/profile.d/path.sh'
+  ruby_block "profile.d" do
+    block do
+      line = 'export PATH=$PATH:/usr/local/go/bin'
+      file = Chef::Util::FileEdit.new("/etc/profile.d/path.sh")
+      file.insert_line_if_no_match("/#{line}/", "#{line}")
+      file.write_file
+    end
+  end
+end
+
+
+ruby_block "bash_profile add" do
+  block do
+    line1 = 'export GOBIN="$HOME/projects/bin"'
+    line2 = 'export GOPATH="$HOME/projects/src"'
+    file = Chef::Util::FileEdit.new(Dir.home + "/.bash_profile")
+    file.insert_line_if_no_match("/#{line1}/", "#{line1}")
+    file.insert_line_if_no_match("/#{line2}/", "#{line2}")
+    file.write_file
+  end
+end
+
+
+# ENV['PATH'] = '$PATH:/usr/local/go/bin'
+# ENV['GOBIN'] = '$HOME/projects/bin'
+# ENV['GOPATH'] = '$HOME/projects/src'
+
+# env 'PATH' do
+#   value '$PATH:/usr/local/go/bin'
+# end
+#
+# env 'GOBIN' do
+#   value '$HOME/projects/bin'
+# end
+#
+# env 'GOPATH' do
+#   value '$HOME/projects/src'
+# end
 
 # bash "install-golang" do
 #   cwd Chef::Config[:file_cache_path]

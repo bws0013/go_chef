8f391e0c16cb65d995979bae421d5ee10cd6acab
Ben <i@auburn.edu>
2018-01-12 20:19:23 -0600

    init for this cookbook

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['go']['version'] = '1.9.2'
+default['go']['platform'] = 'amd64'
+default['go']['filename'] = "go#{node['go']['version']}.#{node['os']}-#{node['go']['platform']}.tar.gz"
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['go']['platform'] = node['kernel']['machine'] =~ /i.86/ ? '386' : 'amd64'
+node.default['go']['filename'] = "go#{node['go']['version']}.#{node['os']}-#{node['go']['platform']}.tar.gz"
+node.default['go']['url'] = "https://golang.org/dl/#{node['go']['filename']}"
+
+execute 'print_stuff' do
+  command "echo #{node.default['go']['url']}"
+end
